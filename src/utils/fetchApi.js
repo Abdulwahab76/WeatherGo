cf2d148e74631399e71b2bde1f87aa63aef3f66a
Abdulwahab76 <64549162+Abdulwahab76@users.noreply.github.com>
2022-11-29 12:30:23 -0800

    changes

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -1,7 +1,7 @@
 export const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'e141dd466emshad832b56164ffd3p1e5d18jsn4f8688ae1c4e',
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
         'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
     }
 };
